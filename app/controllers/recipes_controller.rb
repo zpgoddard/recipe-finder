4632d9c3c403d789a7d8a33f6d2979cf6963e77e
Zac-Goddard-Codices <zpgoddard@gmail.com>
2024-11-22 01:00:57 +0000

    front end

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -3,22 +3,26 @@ class RecipesController < ApplicationController
   end
 
   def search
-    search_terms = params[:ingredients].split(",").map(&:strip).map(&:downcase)
-    like_conditions = search_terms.map { |term| "LOWER(name) LIKE ?" }.join(" OR ")
-    like_values = search_terms.map { |term| "%#{term}%" }
+    if params[:ingredients].present?
+      search_terms = params[:ingredients].split(",").map(&:strip).map(&:downcase)
+      like_conditions = search_terms.map { |term| "LOWER(name) LIKE ?" }.join(" OR ")
+      like_values = search_terms.map { |term| "%#{term}%" }
 
-    matching_ingredients = Ingredient.where(like_conditions, *like_values)
+      matching_ingredients = Ingredient.where(like_conditions, *like_values)
 
-    recipe_matches = Recipe.joins(:recipe_ingredients)
-                           .where(recipe_ingredients: { ingredient_id: matching_ingredients.pluck(:id) })
-                           .select("recipes.*, COUNT(recipe_ingredients.ingredient_id) AS match_count, COUNT(DISTINCT recipe_ingredients.ingredient_id) * 1.0 / COUNT(DISTINCT ingredients.id) AS match_percentage")
-                           .joins(:ingredients)
-                           .group("recipes.id")
-                           .having("COUNT(recipe_ingredients.ingredient_id) > 0")
-                           .order("match_percentage DESC")
-                           .page(params[:page]).per(50) # Paigination via Kaminari gem
+      recipe_matches = Recipe.joins(:recipe_ingredients)
+                             .where(recipe_ingredients: { ingredient_id: matching_ingredients.pluck(:id) })
+                             .select("recipes.*, COUNT(recipe_ingredients.ingredient_id) AS match_count, COUNT(DISTINCT recipe_ingredients.ingredient_id) * 1.0 / COUNT(DISTINCT ingredients.id) AS match_percentage")
+                             .joins(:ingredients)
+                             .group("recipes.id")
+                             .having("COUNT(recipe_ingredients.ingredient_id) > 0")
+                             .order("match_percentage DESC")
+                             .page(params[:page]).per(50) # Pagination via Kaminari gem
 
-    render json: recipe_matches.as_json(include: { recipe_ingredients: { only: [ :amount, :measurement ], methods: :ingredient_name } }, methods: :image_url)
+      render json: recipe_matches.as_json(include: { recipe_ingredients: { only: [ :amount, :measurement ], methods: :ingredient_name } }, methods: :image_url)
+    else
+      render json: []
+    end
   end
 end
 
